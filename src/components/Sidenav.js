a120040089e3d3fe815a00e9a9c32df8e0fca523
juandjara <juanorigami@gmail.com>
2017-03-10 00:59:14 +0100

    feat: sidenav tweak

--- src/components/Sidenav.js
+++ src/components/Sidenav.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import BuildingIcon from 'material-ui/svg-icons/communication/business'
-import FolderIcon from 'material-ui/svg-icons/file/folder'
+import WorkIcon from 'material-ui/svg-icons/action/work'
 import PersonIcon from 'material-ui/svg-icons/social/person'
 import { blue500 } from 'material-ui/styles/colors'
 import Drawer from 'material-ui/Drawer';
@@ -22,7 +22,8 @@ const Sidenav = (props) => {
   }
   return (
     <Drawer width={props.open ? 240 : null}
-            open={props.open}>
+            open={props.open}
+            onRequestChange={(open) => this.setState({open})}>
       <section style={styles.sidenavHeader}>
         <p style={{marginLeft: "1em"}}>Juan Dominguez</p>
       </section>
@@ -34,7 +35,7 @@ const Sidenav = (props) => {
       <Link style={styles.sidenavLink} to="/projects">
         <MenuItem 
           primaryText="Proyectos"
-          leftIcon={<FolderIcon />} />
+          leftIcon={<WorkIcon />} />
       </Link>
       <Link style={styles.sidenavLink} to="/users">
         <MenuItem 
