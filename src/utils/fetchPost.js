fe9804a97d8c48da88f3e3e833aae3296453ee13
juandjara <juanorigami@gmail.com>
2017-05-07 19:57:17 +0200

    refactor: remove fetchPost

--- src/utils/fetchPost.js
+++
@@ -1,9 +0,0 @@
-export default (url, body, headers) => {
-  headers = Object.assign({ 'Content-Type': 'application/json' }, headers);
-  return fetch(url, {
-    method: 'POST',
-    body: JSON.stringify(body),
-    headers
-  })
-  .then(res => res.ok ? res : Promise.reject(res))
-}
