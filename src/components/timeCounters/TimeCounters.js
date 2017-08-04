624687a353b3c5f0ace8295609f88d78a863d392
Juan D. Jara <juanorigami@gmail.com>
2017-08-04 13:52:10 +0200

    feat: add labels to sidenav counters

--- src/components/timeCounters/TimeCounters.js
+++ src/components/timeCounters/TimeCounters.js
@@ -7,16 +7,17 @@ const TooltipText = Tooltip(({children, theme, ...props}) =>
   <span {...props}>{children}</span>)
 
 const timeData = [
-  {time: "00:00:00", icon: "timer", tooltip: "Tiempo actual"},
-  {time: "00:00:00", icon: "today", tooltip: "Tiempo de hoy"},
-  {time: "00:00:00", icon: "date_range", tooltip: "Tiempo de la semana"},
-  {time: "00:00:00", icon: "event_note", tooltip: "Tiempo del mes"},
+  {time: "00:00:00", icon: "timer", tooltip: "Tiempo actual", label: "Ahora"},
+  {time: "00:00:00", icon: "today", tooltip: "Tiempo del dia", label: "Hoy"},
+  {time: "00:00:00", icon: "date_range", tooltip: "Tiempo de la semana", label: "Semana"},
+  {time: "00:00:00", icon: "event_note", tooltip: "Tiempo del mes", label: "Mes"},
 ]
 
-const Counter = ({time, tooltip, icon}) => (
+const Counter = ({time, tooltip, icon, label}) => (
   <div className="time-counters-item">
     <Icon value={icon} />
     <TooltipText tooltip={tooltip}>{time}</TooltipText>
+    <span style={{padding: '0 10px'}}>{label}</span>
   </div>
 )
 
