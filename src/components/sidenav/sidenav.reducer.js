90f912112d153aef88f07274b1f5588344f7cd5b
Juan D. Jara <juanorigami@gmail.com>
2017-09-21 23:51:26 +0200

    fix: sidenav imports and clean unused code

--- src/components/sidenav/sidenav.reducer.js
+++ src/components/sidenav/sidenav.reducer.js
@@ -1,16 +1,13 @@
-import mediaKeys from '../utils/mediaQueries'
+import {MEDIA_CHANGED} from 'redux-mediaquery'
+import mediaKeys from '../../helpers/mediaQueries'
 
 // action type
 export const TOGGLE_SIDENAV_OPEN="TOGGLE_SIDENAV_OPEN"
-export const TOGGLE_SIDENAV_PINNED="TOGGLE_SIDENAV_PINNED"
 
 // action creator
 export function toggleSidenavOpen() {
   return { type: TOGGLE_SIDENAV_OPEN }
-}
-export function toggleSidenavPinned() {
-  return { type: TOGGLE_SIDENAV_PINNED }
-}
+}ç
 
 const initialState = {
   open: false,
@@ -25,15 +22,10 @@ export default (state = initialState, {type, data}) => {
         open: !state.open,
         pinned: state.pinned
       }
-    case '@@rdx-mqt/MEDIA_CHANGED':
+    case MEDIA_CHANGED:
       return {
         open: state.open,
         pinned: !data.small
-      }
-    case TOGGLE_SIDENAV_PINNED:
-      return {
-        pinned: !state.pinned,
-        open: state.open
       }
     default:
       return state
