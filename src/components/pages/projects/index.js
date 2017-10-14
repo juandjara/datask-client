8d6e7e624e94bbb1cdcd0fc1249e05d3aede322c
Juan D. Jara <juanorigami@gmail.com>
2017-10-15 01:26:47 +0200

    feat: remove unused files for project

--- src/components/pages/projects/index.js
+++ src/components/pages/projects/index.js
@@ -1,7 +1,6 @@
 import ProjectForm from './ProjectForm'
-import ProjectFormDialog from './ProjectFormDialog'
 import ProjectList from './Projects'
 import ProjectUser from './ProjectUsers'
 import './Projects.css'
 
-export {ProjectForm, ProjectFormDialog, ProjectList, ProjectUser}
+export {ProjectForm, ProjectList, ProjectUser}
