c5e873d94aac41626779e62a96953e4963d17643
Juan D. Jara <juanorigami@gmail.com>
2017-11-16 21:17:19 +0100

    feat: profile reducer reacts to create and finish time

--- src/reducers/profile.reducer.js
+++ src/reducers/profile.reducer.js
@@ -1,5 +1,6 @@
 import axios from 'services/axiosWrapper';
 import toast from 'services/toastWrapper'
+import { types as timeTypes } from 'reducers/time.reducer'
 
 // action types
 export const PROFILE_FETCH = "PROFILE_FETCH"
@@ -22,17 +23,34 @@ export function saveProfile(profile) {
 }
 
 // reducer
-export default (state = {loading: false}, action) => {
-  switch (action.type) {
+export default (state = {loading: false}, action = {}) => {
+  const {type, payload = {}} = action 
+  switch (type) {
     case `${PROFILE_FETCH}_LOADING`:
     case `${PROFILE_UPDATE}_LOADING`:
       return { ...state, loading: true }
     case `${PROFILE_FETCH}_SUCCESS`:
     case `${PROFILE_UPDATE}_SUCCESS`:
-      return { ...action.payload, loading: false }
+      return { ...payload, loading: false }
     case `${PROFILE_FETCH}_ERROR`:
     case `${PROFILE_UPDATE}_ERROR`:
-      return { ...state, error: errMapper(action.payload), loading: false }
+      return { 
+        ...state, 
+        error: errMapper(payload), 
+        loading: false 
+      }
+    case `${timeTypes.CREATE}_SUCCESS`:
+      return {
+        ...state,
+        activeTask: payload.task,
+        activeTimeStart: payload.startTime
+      }
+    case `${timeTypes.FINISH}_SUCCESS`:
+      return {
+        ...state,
+        activeTask: null,
+        activeTimeStart: null
+      }    
     default:
       return state;
   }
