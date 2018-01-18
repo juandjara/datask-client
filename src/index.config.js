795ea99bf9759d1a10d4d305d17947ece23898a4
Juan D. Jara <juanorigami@gmail.com>
2018-01-18 22:13:40 +0100

    back to api

--- src/index.config.js
+++ src/index.config.js
@@ -1,6 +1,6 @@
 export default {
-  //api: 'https://datask-api.now.sh',
-  api: 'http://localhost:8080',
+  api: 'https://datask-api.now.sh',
+  // api: 'http://localhost:8080',
   mediaQueries: {
     small: 'screen and (max-width: 900px)',
     mobile: 'screen and (max-width: 600px)',
