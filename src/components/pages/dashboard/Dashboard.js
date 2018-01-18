b808b57ef331a2d6dd17c92629d47d31821e3c85
Juan D. Jara <juanorigami@gmail.com>
2018-01-18 22:14:00 +0100

    user selector in dashboard

--- src/components/pages/dashboard/Dashboard.js
+++ src/components/pages/dashboard/Dashboard.js
@@ -1,15 +1,70 @@
 import React, {Component} from 'react'
+import { renderAsyncSelect as AsyncSelect } from '../../shared/FormFields';
+import { searchUsers } from 'services/selectHelpers'
+import styled from 'styled-components';
+import {connect} from 'react-redux'
 
-export default class Dashboard extends Component {
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  .select-icon {
+    line-height: 60px;
+    padding: 12px;
+    padding-bottom: 0;
+  }
+  .select-container {
+    min-width: 230px;
+  }
+`;
+
+class Dashboard extends Component {
+  state = {
+    selectedUser: {}
+  }
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.selectedUser.value !== this.state.selectedUser.value) {
+      this.setState({selectedUser: nextProps.selectedUser})
+    }
+  }
   render() {
     const loading = true;
+    const searchInputProps = {
+      value: this.state.selectedUser,
+      onChange: (selectedUser) => this.setState({selectedUser})
+    }
     return  (
       <div className="list-container">
         <div className="list-title-container">
-          <h2 className="list-title">Dashboard</h2>
+          <Header>
+            <h2 className="list-title">Reportes</h2>
+            {this.props.isAdmin && (
+              <AsyncSelect
+                name="user"
+                icon="person"
+                label="Usuario"
+                className="select"
+                placeholder="Escribe para buscar"
+                loadOptions={searchUsers}
+                input={searchInputProps}
+                meta={{}}
+              />
+            )}
+          </Header>
           {loading && <p className="color-primary">Cargando ... </p>}
         </div>
       </div>
     )
   }
 }
+
+export default connect(
+  (state, props) => {
+    return {
+      isAdmin: state.auth.roles.indexOf("ADMIN") !== -1,
+      selectedUser: {
+        label: state.auth.full_name,
+        value: state.auth._id
+      }
+    }
+  }
+)(Dashboard)
